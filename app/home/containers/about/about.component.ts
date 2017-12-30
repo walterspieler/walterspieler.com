89923a950f6cd1ef2c3dfd64336e30c30a34aa91
walterspieler <matthieu@walterspieler.com>
2017-12-30 18:24:07 +0100

    Repsonsive && Favicon & Minor changes

--- app/home/containers/about/about.component.ts
+++ app/home/containers/about/about.component.ts
@@ -5,25 +5,28 @@ import { Component } from '@angular/core';
 	styleUrls: ['about.component.scss'],
 	template: `
 	<div class="about container" fxLayout="row" fxLayoutWrap fxLayoutAlign="center center">
-		<mat-card class="item" fxFlex="50">
+		<mat-card class="item" fxFlex="50" fxFlex.sm="75" fxFlex.xs="100">
 			<div>
 				<div class="mat-display-2" style="text-transform: uppercase;font-weight: 700;margin-bottom: .25em;">Matt Walterspieler</div>
 			</div>
-			<mat-card-header>
+			<mat-card-header class="bio">
 				<img mat-card-avatar src="/assets/matt.jpg">
-				<mat-card-title style="margin: 4px 0;">Fullstack Webdeveloper</mat-card-title>
-				<mat-card-subtitle><span style="color:black;">🇫🇷</span> Paris</mat-card-subtitle>
+				<mat-card-title>Fullstack Webdeveloper</mat-card-title>
+				<mat-card-subtitle>Paris <span style="color:black;">🇫🇷 🇪🇺</span></mat-card-subtitle>
 			</mat-card-header>
 			<mat-card-content>
 				<mat-list dense>
-					<h3 matSubheader>Skills</h3>
+					<h3 matSubheader>Front-end</h3>
 					<mat-divider></mat-divider>
 					<mat-list-item>Javascript (ES 6)</mat-list-item>
 					<mat-list-item>Angular & Angularjs</mat-list-item>
+					<h3 matSubheader>Back-end</h3>
+					<mat-divider></mat-divider>
 					<mat-list-item>Node.js</mat-list-item>
+					<h3 matSubheader>Deployment</h3>
+					<mat-divider></mat-divider>
 					<mat-list-item>Docker</mat-list-item>
 					<mat-list-item>Kubernetes (K8s)</mat-list-item>
-					<mat-divider></mat-divider>
 				</mat-list>
 			</mat-card-content>
 			<mat-card-actions fxLayoutAlign="end end">
