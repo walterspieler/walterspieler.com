38fd1a49da73452454a33148c5d3683ec76319dd
walterspieler <matthieu@walterspieler.com>
2018-01-20 15:29:12 +0100

    Https certifcates enabled

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
 'use strict';
 let express = require('express');
+let httpsRedirect = require('express-https-redirect');
 let app = express();
 let bodyParser = require('body-parser');
 
-let port = 3000;
+let port = process.env.NODE_ENV === 'production' ? 443 : 3000;
 
 //EXPRESS CONFIG
+app.use('/', httpsRedirect());
 app.use(express.static('.'));
 app.use(bodyParser.urlencoded({
-	extended: false,
-	limit: '5mb'
+  extended: false,
+  limit: '5mb'
 }));
 app.use(bodyParser.json({
-	limit: '5mb'
+  limit: '5mb'
 }));
 //END EXPRESS CONFIG
 
 app.listen(port, function() {
-	console.log(process.env.NODE_ENV);
-	console.log('App listening on port 3000!');
+  console.log(process.env.NODE_ENV);
+  console.log(`App listening on port ${port}!`);
 });
