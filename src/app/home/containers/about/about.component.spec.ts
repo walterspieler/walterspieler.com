db17590637ada72a7bfbd48bcac3424b98c9025f
Matt Walterspieler <matthieu@walterspieler.com>
2019-03-22 14:07:31 +0100

    feat: add Medium link, chore: Updated packages

--- src/app/home/containers/about/about.component.spec.ts
+++ src/app/home/containers/about/about.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
+import { MaterialModule } from '../../../material/material.module';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -8,9 +9,10 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      imports: [MaterialModule],
+      declarations: [AboutComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
